083d1e24ebeeae0dabc57dea837a580e8be95ef4
inaa123 <dlsdk7912@naver.com>
2023-12-29 17:29:39 +0900

    검색 수정

--- src/components/SearchBook.jsx
+++ src/components/SearchBook.jsx
@@ -23,7 +23,7 @@ function SearchBook() {
         setIsLoading(true);
         setError(null);
         try {
-            const apiUrl = `/?query=${encodeURIComponent(keywords)}`;
+            const apiUrl = `/api/books/?query=${encodeURIComponent(keywords)}`;
             const res = await axios.get(apiUrl);
             if (res.headers['content-type']?.includes('application/json')) {
                 const { data } = res;
