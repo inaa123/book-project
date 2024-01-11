7b360ff0dd19928ac5dd2ba8018ab2fc801d6d71
inaa123 <dlsdk7912@naver.com>
2024-01-11 21:57:58 +0900

    swiper 추가

--- src/pages/MyBook.jsx
+++ src/pages/MyBook.jsx
@@ -38,25 +38,17 @@ function MyBook() {
     return (
         <MyBookContainer className='container'>
             <MyBookStateCategory state={state} onSelect={onSelect} />
-            <Swiper
-                slidesPerView={8}
-                slidesPerGroup={8}
-                loop
-            >
+            
             <ul>
                 
                 <li>
                 {!bookList && <p>{msg}</p>}
                 {bookList && bookList.map((el,index) => (
-                    <SwiperSlide key={index}>
                         <MyBookListItem key={el.id} post={el} state={state} />
-                    </SwiperSlide>
                     
                 ))}
                 </li>
             </ul>
-            
-            </Swiper>
         </MyBookContainer>
     )
 }
